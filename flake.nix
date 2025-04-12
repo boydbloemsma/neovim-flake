f6b062926a8c3dfdab4feba7a57c0f3f7afd3d10
Boyd Bloemsma <boydbloemsma@gmail.com>
2025-04-12 18:51:31 +0200

    fix: reformatting

--- flake.nix
+++ flake.nix
@@ -1,23 +1,23 @@
 {
-  description = "NVF configuration flake";
+    description = "NVF configuration flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nvf.url = "github:notashelf/nvf";
-  };
-
-  outputs = { self, nixpkgs, nvf, ... }: {
-
-    packages.x86_64-linux.default = 
-      (nvf.lib.neovimConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-	modules = [ ./nvf-configuration.nix ];
-      }).neovim;
-
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      modules = [
-        nvf.nixosModules.default
-      ];
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+        nvf.url = "github:notashelf/nvf";
     };
-  };
+
+    outputs = { self, nixpkgs, nvf, ... }: {
+
+        packages.x86_64-linux.default = 
+            (nvf.lib.neovimConfiguration {
+                pkgs = nixpkgs.legacyPackages.x86_64-linux;
+                modules = [ ./nvf-configuration.nix ];
+            }).neovim;
+
+        nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+            modules = [
+                nvf.nixosModules.default
+            ];
+        };
+    };
 }
