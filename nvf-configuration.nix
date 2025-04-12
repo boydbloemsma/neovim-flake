0f3a1015a1d46502376f7d68c0955663efb645f2
Boyd Bloemsma <boydbloemsma@gmail.com>
2025-04-12 16:15:20 +0200

    feat: added some language servers

--- nvf-configuration.nix
+++ nvf-configuration.nix
@@ -1,9 +1,28 @@
 { pkgs, lib, ... }:
 
 {
-  vim.theme.enable = true;
-  vim.theme.name = "gruvbox";
-  vim.theme.style = "light";
 
-  vim.languages.nix.enable = true;
+        vim = {
+                theme = {
+                        enable = true;
+                        name = "gruvbox";
+                        style = "light";
+                };
+
+                statusline.lualine.enable = true;
+                telescope.enable = true;
+                autocomplete.nvim-cmp.enable = true;
+
+                languages = {
+                        enableLSP = true;
+                        enableTreesitter = true;
+
+                        nix.enable = true;
+                        php.enable = true;
+                        lua.enable = true;
+                        html.enable = true;
+                        markdown.enable = true;
+                        ts.enable = true;
+                };
+        };
 }
